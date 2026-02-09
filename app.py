b437786c321a177d0a846ffc47c6a40b0f95119d
tubagusalwasii <140483973+tubagusalwasii@users.noreply.github.com>
2026-02-09 12:40:06 +0700

    Refactor imports and update API model

--- app.py
+++ app.py
@@ -67,7 +67,8 @@ if st.button("Mulai Analisis"):
         try:
             # 1. Inisialisasi Gemini 3
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('models/gemini-1.5-flash-latest')
+            model = genai.GenerativeModel("gemini-flash-latest")
+
             
             with st.spinner(f"Mengambil data {ticker_input}..."):
                 # 2. Ambil Data dari Yahoo Finance
@@ -129,6 +130,7 @@ if st.button("Mulai Analisis"):
 st.divider()
 
 st.caption("Aplikasi ini dibuat untuk tujuan edukasi. Keputusan investasi ada di tangan Anda sendiri (DYOR).")
+
 
 
 
